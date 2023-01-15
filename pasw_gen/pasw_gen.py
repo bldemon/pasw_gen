6dfc6a550d58cf3ab878412d62a35f8a1b177416
bldemon <bldemon@yandex.ru>
2023-01-15 21:12:21 +0300

    add answer

--- pasw_gen/pasw_gen.py
+++ pasw_gen/pasw_gen.py
@@ -3,3 +3,12 @@ lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation =  '!#$%&*+-=?@^_'
 chars = ''
+
+
+Количество паролей для генерации;
+Длину одного пароля;
+Включать ли цифры 0123456789?
+Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?
+Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?
+Включать ли символы !#$%&*+-=?@^_?
+Исключать ли неоднозначные символы il1Lo0O?
