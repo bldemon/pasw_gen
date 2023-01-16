f384169ae063476ed55515934c9f610c22f41ee2
bldemon <bldemon@yandex.ru>
2023-01-16 21:44:07 +0300

    add code1

--- pasw_gen/pasw_gen.py
+++ pasw_gen/pasw_gen.py
@@ -1,5 +1,12 @@
 import random
 
+def validation_symbol(symbol):
+    if symbol not in 1234567890:
+        print('Введите цифру:')
+def validation_char(char):
+    if char not in 'y' or 'n':
+        print('Введите правильный символ:')
+
 
 
 digits = '0123456789'
@@ -8,11 +15,19 @@ uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation =  '!#$%&*+-=?@^_'
 chars = ''
 
+print('Добро пожаловать в генератор паролей')
+print('Укажите длину одного пароля:')
+password_lenght = int(input())
+validation_symbol(password_lenght)
+print('Включать ли цифры 0123456789? y/n')
+password_digit = input()
+print('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? y/n')
+password_upper_case = input()
+print('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? y/n')
+password_lower_case = input()
+print('Включать ли символы !#$%&*+-=?@^_? y/n')
+password_symbols = input()
+print('Исключать ли неоднозначные символы il1Lo0O? y/n')
+password_similar_symbols = input()
 
-Количество паролей для генерации;
-Длину одного пароля;
-Включать ли цифры 0123456789?
-Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?
-Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?
-Включать ли символы !#$%&*+-=?@^_?
-Исключать ли неоднозначные символы il1Lo0O?
+
