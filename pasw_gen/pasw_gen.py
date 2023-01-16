c73ef65c5a579232f136fd9fff4343e1c4ccd66e
bldemon <bldemon@yandex.ru>
2023-01-16 23:13:21 +0300

    add def password_generate

--- pasw_gen/pasw_gen.py
+++ pasw_gen/pasw_gen.py
@@ -25,24 +25,31 @@ def validation_char():
             return 0
 
 
-digits = '0123456789'
-lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-punctuation =  '!#$%&*+-=?@^_'
-chars = ''
+def password_generate():
+    digits = '0123456789'
+    lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+    uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    punctuation = '!#$%&*+-=?@^_'
+    chars = ''
+
+
 print('Добро пожаловать в генератор паролей')
+
 print('Укажите количество паролей для генерации:')
 password_number = validation_symbol()
 print('Укажите длину одного пароля:')
 password_lenght = validation_symbol()
-print('Включать ли цифры 0123456789? y/n')
+print('Включить в пороль цифры 0123456789? y/n')
 password_digit = validation_char()
-print('Включать ли прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? y/n')
+print('Включить в пароль прописные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ? y/n')
 password_upper_case = validation_char()
-print('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz? y/n')
+print('Включить в пароль строчные буквы abcdefghijklmnopqrstuvwxyz? y/n')
 password_lower_case = validation_char()
-print('Включать ли символы !#$%&*+-=?@^_? y/n')
+print('Включить в пароль символы !#$%&*+-=?@^_? y/n')
 password_symbols = validation_char()
-print('Исключать ли неоднозначные символы il1Lo0O? y/n')
+print('Исключить из пароля похожие символы iI1lLoO0? y/n')
 password_similar_symbols = validation_char()
+
+
+
 
